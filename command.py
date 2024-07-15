010e78c79c3e2b4bf09d8439bbe58f1409f62cd7
sumin1s.song <sumin1s.song@samsung.com>
2024-07-15 16:58:37 +1000

    [Refactor] format 맞춤

--- command.py
+++ command.py
@@ -10,18 +10,20 @@ class Command(ABC):
 
 
 class ReadCommand(Command):
-    def __init__(self, index:int):
+    def __init__(self, index: int):
         self.index = index
 
     def run(self):
         print("ReadCommand.run()")
         subprocess.run(["python", "ssd.py", "R", self.index])
 
+
 class FullReadCommand(Command):
     def run(self):
         print("FullReadCommand.run()")
         for index in range(100):
             subprocess.run(["python", "ssd.py", "R", index])
+
 
 class ExitCommand(Command):
     def run(self):
--- test/test_command_read.py
+++ test/test_command_read.py
@@ -6,6 +6,8 @@ from shell import Shell
 
 INDEX_0 = 0
 INDEX_10 = 10
+
+
 class TestCommandRead(TestCase):
     @patch('subprocess.run')
     def test_read_index_0(self, mock_run):
@@ -20,14 +22,7 @@ class TestCommandRead(TestCase):
         mock_run.assert_called_once_with(["python", 'ssd.py', 'R', INDEX_10])
 
     @patch('subprocess.run')
-    def test_fullread(self,mock_run):
+    def test_fullread(self, mock_run):
         fullread_command = FullReadCommand()
         fullread_command.run()
         self.assertEqual(100, mock_run.call_count)
-
-
-
-
-
-
-
