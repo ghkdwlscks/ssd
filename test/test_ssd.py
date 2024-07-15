d46bac0f26e77dbe9b10058672993291f1cf623a
hodri <kanggeunpyo@gmail.com>
2024-07-15 16:57:29 +0900

    [ADD] SSD.py 테스트코드 추가

--- test/test_ssd.py
+++ test/test_ssd.py
@@ -1,16 +1,25 @@
+import os
 from unittest import TestCase
 from unittest.mock import Mock
 
 from console import Console
 from ssd import SSD
 
-RESULT_FILE = '../output/result.txt'
-NAND_FILE = '../output/nand.txt'
+RESULT_FILE = '../output/result_test.txt'
+NAND_FILE = '../output/nand_test.txt'
 
 
 class TestSSD(TestCase):
     def setUp(self):
+        os.environ['NAND_TXT_PATH'] = 'output\\nand_test.txt'
+        os.environ['RESULT_TXT_PATH'] = 'output\\result_test.txt'
         self.sut = SSD()
+
+    def tearDown(self):
+        if 'NAND_TXT_PATH' in os.environ:
+            del os.environ['NAND_TXT_PATH']
+        if 'RESULT_TXT_PATH' in os.environ:
+            del os.environ['RESULT_TXT_PATH']
 
     def test_ssd_set_command(self):
         self.sut.set_command('R', 5)
@@ -24,27 +33,25 @@ class TestSSD(TestCase):
             self.sut.set_command('Read', 5)
 
     def test_ssd_read_empty_nand(self):
-        self.refresh_nand()
+        self.sut.refresh_nand()
         self.sut.set_command('R', 5)
         self.sut.run()
+
+        self.assertEqual('0x00000000', self.sut.check_result_txt())
 
     def test_ssd_write(self):
-        self.refresh_nand()
+        self.sut.refresh_nand()
         self.sut.set_command('W', 5, "0xAB010105")
-        self.run()
+        self.sut.run()
 
-        self.assertEqual('0xAB010105', self.check_nand_txt(5))
+        self.assertEqual('0xAB010105', self.sut.check_nand_txt(5))
 
     def test_ssd_write_and_read(self):
-        self.refresh_nand()
+        self.sut.refresh_nand()
         self.sut.set_command('W', 5, "0xAB010105")
         self.sut.run()
 
         self.sut.set_command('R', 5)
         self.sut.run()
 
-        with open(RESULT_FILE, 'r') as f:
-            content = f.readline()
-            lba, data = content.split()
-        self.assertEqual(5, lba)
-        self.assertEqual("0xAB010105", data)
+        self.assertEqual("0xAB010105", self.sut.check_result_txt())
