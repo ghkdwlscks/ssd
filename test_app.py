2254542945bf1a9377ac4b1a30ef0805300649f6
Jinchan <ghkdwlscks@gmail.com>
2024-07-16 13:56:48 +0900

    [Feat] TestApp1: implement TestApp1
    Signed-off-by: Jinchan <ghkdwlscks@gmail.com>

--- test_app.py
+++ test_app.py
@@ -1,3 +1,6 @@
+import io
+import sys
+
 from command import make_command
 
 
@@ -5,37 +8,45 @@ class TestApp:
     def __init__(self, filename: str) -> None:
         self.filename = filename
         self.commands = self.__read_script_file()
+        self.output = None
 
     def __read_script_file(self) -> list[str]:
         with open(self.filename) as file:
             return [line.strip() for line in file]
 
     def run(self):
+        print(f"Running {self.__class__.__name__} ..")
+        old_stdout = sys.stdout
+        self.output = io.StringIO()
+        sys.stdout = self.output
         for command in self.commands:
-            self.__run_command(command)
-        if self.__validate():
+            self._run_command(command)
+        is_valid = self._validate()
+        sys.stdout = old_stdout
+        if is_valid:
             print(f"{self.__class__.__name__} passed")
         else:
             print(f"{self.__class__.__name__} failed")
 
     @staticmethod
-    def __run_command(command: str) -> None:
+    def _run_command(command: str) -> None:
         command = make_command(command.strip())
         command.run()
 
-    def __validate(self) -> bool:
+    def _validate(self) -> bool:
         pass
 
 
 class TestApp1(TestApp):
 
-    def __validate(self) -> bool:
-        pass
+    def _validate(self) -> bool:
+        self._run_command("fullread")
+        return len(set(self.output.getvalue().splitlines())) == 1
 
 
 class TestApp2(TestApp):
 
-    def __validate(self) -> bool:
+    def _validate(self) -> bool:
         pass
 
 
