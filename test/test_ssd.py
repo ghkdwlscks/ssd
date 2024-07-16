34161fe1e9f88ac6a055c1941e226fdb989e269c
sumin1s.song <sumin1s.song@samsung.com>
2024-07-16 14:56:19 +1000

    [refactor] use NUM_LBA instead 100 and use INDEX_5 instead 5

--- test/test_ssd.py
+++ test/test_ssd.py
@@ -4,8 +4,9 @@ from unittest.mock import Mock
 
 from ssd import SSD
 
-FULL_READ_FILE = '\n'.join(['0x00000000' for _ in range(100)])
-
+NUM_LBA = 100
+INDEX_5 = 5
+FULL_READ_FILE = '\n'.join(['0x00000000' for _ in range(NUM_LBA)])
 
 class TestSSD(TestCase):
     def setUp(self):
@@ -22,19 +23,19 @@ class TestSSD(TestCase):
             del os.environ['RESULT_TXT_PATH']
 
     def test_ssd_set_command(self):
-        self.sut.set_command('R', 5)
+        self.sut.set_command('R', INDEX_5)
 
     def test_ssd_set_command_fail_out_of_lba_range(self):
         with self.assertRaises(AttributeError):
-            self.sut.set_command('R', 100)
+            self.sut.set_command('R', NUM_LBA)
 
     def test_ssd_set_command_wrong_cmd(self):
         with self.assertRaises(AttributeError):
-            self.sut.set_command('Read', 5)
+            self.sut.set_command('Read', INDEX_5)
 
     def test_ssd_read_empty_nand(self):
         self.sut.refresh_nand()
-        self.sut.set_command('R', 5)
+        self.sut.set_command('R', INDEX_5)
         self.sut.run()
         self.result_txt_file.return_value = '0x00000000'
 
@@ -42,19 +43,19 @@ class TestSSD(TestCase):
 
     def test_ssd_write(self):
         self.sut.refresh_nand()
-        self.sut.set_command('W', 5, "0xAB010105")
+        self.sut.set_command('W', INDEX_5, "0xAB010105")
         self.sut.run()
-        self.nand_txt_file.side_effect = lambda x: '0xAB010105' if x == 5 else '0x00000000'
+        self.nand_txt_file.side_effect = lambda x: '0xAB010105' if x == INDEX_5 else '0x00000000'
 
-        self.assertEqual('0xAB010105', self.nand_txt_file(5))
+        self.assertEqual('0xAB010105', self.nand_txt_file(INDEX_5))
 
     def test_ssd_write_and_read(self):
         self.sut.refresh_nand()
-        self.sut.set_command('W', 5, "0xAB010105")
+        self.sut.set_command('W', INDEX_5, "0xAB010105")
         self.sut.run()
-        self.nand_txt_file.side_effect = lambda x: '0xAB010105' if x == 5 else '0x00000000'
+        self.nand_txt_file.side_effect = lambda x: '0xAB010105' if x == INDEX_5 else '0x00000000'
 
-        self.sut.set_command('R', 5)
+        self.sut.set_command('R', INDEX_5)
         self.sut.run()
         self.result_txt_file.return_value = '0xAB010105'
 
