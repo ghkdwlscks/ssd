4fb202b246ed27a561c9905872c1f46b945a7cd7
sanghyung.kim <sh03.kim@samsung.com>
2024-07-17 13:49:41 +0900

    [Add] console에 log 구문 추가

--- console.py
+++ console.py
@@ -1,23 +1,29 @@
 import os
 
+from logger import Logger
+
 
 class Console:
     def __init__(self):
         dir_path = os.path.dirname(__file__)
         self.file_path = os.path.join(dir_path, "output", "result.txt")
+        self.logger = Logger()
 
     def is_exist_result_file(self):
         return os.path.exists(self.file_path)
 
     def read_result_file(self):
         try:
+            self.logger.log(self, "read_result_file()", "result.txt read")
             with open(self.file_path, 'r') as file:
                 data = file.read().strip()
         except FileNotFoundError:
+            self.logger.log(self, "read_result_file()", "Fail - FileNotFoundError 발생")
             data = ""
         return data
 
     def read(self):
+        self.logger.log(self, "read()", "console read")
         if not self.is_exist_result_file():
             return
         if not (result := self.read_result_file()):
@@ -25,6 +31,7 @@ class Console:
         print(result)
 
     def write(self, data):
+        self.logger.log(self, "write()", "console write")
         if not self.is_exist_result_file():
             return
 
