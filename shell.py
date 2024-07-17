19425bf6ad5eaf9d7891c939e33c848041f75e7f
hodri <kanggeunpyo@gmail.com>
2024-07-17 09:59:30 +0900

    [add] EraseCommand, EraseRangeCommand 추가

--- shell.py
+++ shell.py
@@ -33,6 +33,10 @@ class Shell:
             return True
         if re.fullmatch(r"fullwrite 0x[0-9A-F]{8}", command):
             return True
+        if re.fullmatch(r"erase [0-9]{1,2} \d+$", command):
+            return True
+        if re.fullmatch(r"erase_range [0-9]{1,2} [0-9]{1,2}", command):
+            return True
         return False
 
     @staticmethod
