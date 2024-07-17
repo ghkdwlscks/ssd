f75e00781ff3f2345eac6d063495f516e6ddbd66
hyowon.moon <hyowon.moon@samsung.com>
2024-07-17 10:52:50 +0900

    [Add] new senario (test app) case

--- test_app.py
+++ test_app.py
@@ -5,28 +5,56 @@ from command import make_command
 
 
 class TestApp:
-    def __init__(self, filename: str) -> None:
-        self.filename = filename
-        self.commands = self.__read_script_file()
+    def __init__(self, test_name: str) -> None:
+        self.test_name = test_name
+        self.file_name = f"script/{test_name}.txt"
+        self.commands, self.answer = self.__read_script_file()
         self.output = None
 
-    def __read_script_file(self) -> list[str]:
-        with open(self.filename) as file:
-            return [line.strip() for line in file]
+    def __parse_command_answer_file(self, lines: list[str]):
+        commands = []
+        answer = []
+
+        command_flag = 0
+        answer_flag = 0
+
+        for line in lines:
+            if line.startswith('#') and 'command' in line.lower():
+                command_flag = 1
+                answer_flag = 0
+            elif line.startswith('#') and 'answer' in line.lower():
+                command_flag = 0
+                answer_flag = 1
+            elif command_flag == 1:
+                commands.append(line)
+            elif answer_flag == 1:
+                answer.append(line)
+
+        return commands, '\n'.join(answer)
+
+    def __read_script_file(self):
+        with open(self.file_name, 'r') as file:
+            lines = [line.strip() for line in file]
+        return self.__parse_command_answer_file(lines)
 
     def run(self):
-        print(f"Running {self.__class__.__name__} ..")
+        print(f"{self.test_name} --- Run...", end='')
+
         old_stdout = sys.stdout
         self.output = io.StringIO()
         sys.stdout = self.output
+
         for command in self.commands:
             self._run_command(command)
+
         is_valid = self._validate()
         sys.stdout = old_stdout
         if is_valid:
-            print(f"{self.__class__.__name__} passed")
+            print(f"Pass")
+            return True
         else:
-            print(f"{self.__class__.__name__} failed")
+            print(f"FAIL!")
+            return False
 
     @staticmethod
     def _run_command(command: str) -> None:
@@ -34,24 +62,4 @@ class TestApp:
         command.run()
 
     def _validate(self) -> bool:
-        pass
-
-
-class TestApp1(TestApp):
-
-    def _validate(self) -> bool:
-        return len(set(self.output.getvalue().splitlines())) == 1
-
-
-class TestApp2(TestApp):
-
-    def _validate(self) -> bool:
-        answer = "0x12345678\n0x12345678\n0x12345678\n0x12345678\n0x12345678\n0x12345678"
-        return self.output.getvalue().strip() == answer
-
-
-def make_test_app(command: str) -> TestApp:
-    if command == "testapp1":
-        return TestApp1("script/test_app1.txt")
-    elif command == "testapp2":
-        return TestApp2("script/test_app2.txt")
+        return self.output.getvalue().strip() == self.answer.strip()
